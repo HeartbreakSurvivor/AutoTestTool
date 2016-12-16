0bcc876704c5aaf8b27a008265c5bd5ba44d5d70
bigzhanghao <839677080@qq.com>
2016-12-16 21:47:13 +0800

    mofify the srial module

--- Serial.py
+++ Serial.py
@@ -1,7 +1,8 @@
 import threading
 
 from serial import Serial
-
+from time import sleep
+from mainboard import Ui_UsartTool
 
 class MySerial(threading.Thread):
     def __init__(self):
@@ -9,6 +10,7 @@ class MySerial(threading.Thread):
         self.terminate = False
 
     def open(self,settings):
+        self.serial.port = Ui_UsartTool.co
         try:
             self.serial = Serial(settings["Port"],)
             self.serial.flushInput()
@@ -25,3 +27,24 @@ class MySerial(threading.Thread):
 
     def send(self,data,_type):
         self.serial.write(data)
+
+    @property
+    def receive(self):
+        data, quit = None, False
+        while 1:
+            data = self.serial.read(1)
+            if data == '':
+                continue
+            while 1:
+                n = self.serial.inWaiting()
+                if n > 0:
+                    data = "%s%s" % (data, self.serial.read(n))
+                    sleep(0.02) # data is this interval will be merged
+                else:
+                    quit = True
+                    break
+            if quit:
+                break
+        return data
+
+    def PortList(self):
