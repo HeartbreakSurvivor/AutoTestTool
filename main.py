2d638107b3909c0ebd1e65d043d1c0364edd27d9
bigzhanghao <839677080@qq.com>
2017-02-12 12:42:13 +0800

    the new main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys,binascii
 import importlib
 importlib.reload(sys)
 
+from keyedit import Ui_KeyEdit
 from mainboard import Ui_UsartTool
 from PyQt4 import QtCore, QtGui
 from serial import Serial
@@ -40,6 +41,48 @@ def hexshow(argv):
         result += hhex + ' '
         #print("hexshow: ", result)
     return result
+
+class KeyEdit(QtGui.QWidget,Ui_KeyEdit):
+    def __init__(self,parent=None):
+        super().__init__(parent)
+
+        self.__Customize = list[self.KeyCustome1,self.KeyCustome2,self.KeyCustome3,self.KeyCustome4,
+                                self.KeyCustome5,self.KeyCustome6,self.KeyCustome7]
+        self.__Content = list[self.SendMsg1,self.SendMsg2,self.SendMsg3,self.SendMsg4,self.SendMsg5,
+                              self.SendMsg6,self.SendMsg7]
+        self.__VirtualKey = list[self.VirtualKey1,self.VirtualKey2,self.VirtualKey3,self.VirtualKey4,
+                                self.VirtualKey5,self.VirtualKey6,self.VirtualKey7]
+        self.__KeyMsg = list[Keymsg_1,Keymsg_2,Keymsg_3,Keymsg_4,Keymsg_5,Keymsg_6,Keymsg_7]
+
+        self.KeyCustome1.connect(self.KeyCustome1, QtCore.SIGNAL('clicked()'), self.IsCustomized)
+        self.KeyCustome2.connect(self.KeyCustome2, QtCore.SIGNAL('clicked()'), self.IsCustomized)
+        self.KeyCustome3.connect(self.KeyCustome3, QtCore.SIGNAL('clicked()'), self.IsCustomized)
+        self.KeyCustome4.connect(self.KeyCustome4, QtCore.SIGNAL('clicked()'), self.IsCustomized)
+        self.KeyCustome5.connect(self.KeyCustome5, QtCore.SIGNAL('clicked()'), self.IsCustomized)
+        self.KeyCustome6.connect(self.KeyCustome6, QtCore.SIGNAL('clicked()'), self.IsCustomized)
+        self.KeyCustome7.connect(self.KeyCustome7, QtCore.SIGNAL('clicked()'), self.IsCustomized)
+
+
+    def IsCustomized(self):
+        for i in range(0,self.__Customize.__len__()):
+            if self.__Customize[i].isChecked():
+                self.__KeyMsg[i].isCustomize = 1
+                self.__Content[i].setReadOnly(True)
+            else:
+                self.__KeyMsg[i].isCustomize = 0
+                self.__Content[i].setReadOnly(False)
+
+    def GetEntityKey(self):
+        for i in range(0,self.__VirtualKey.__len__()):
+            if self.__VirtualKey.count(self.__VirtualKey[i].CurrentText()) > 1:
+                QtGui.QMessageBox.information(self, "Tips", "定义了相同的按键")
+                break
+            if self.__VirtualKey[i].CurrentText() is not None:
+                self.__KeyMsg[i].setEntityKey(self.__VirtualKey[i].CurrentText())
+
+    def GetSendMsg(self):
+
+
 
 class MainWidget(QtGui.QWidget,Ui_UsartTool):
     def __init__(self,parent=None):
